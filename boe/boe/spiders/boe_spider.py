0ccffa637f1263d6b8458990415b16b5f9445ec4
Andrea Varela Fernández <andrea.varela.fernandez@udc.es>
2025-09-11 13:08:12 +0200

    feat: scrraping desde pestaña de día dado

--- boe/boe/spiders/boe_spider.py
+++ boe/boe/spiders/boe_spider.py
@@ -2,19 +2,45 @@ import scrapy
 
 class BOESpider(scrapy.Spider):
     name = "boe"
-
-    start_urls = ["https://boe.es/rss/boe.php?s=1"]
+    start_urls = ["https://boe.es/boe/dias/2025/09/10/"]
 
     def parse(self, response):
-        for item in response.css("item"):
-            texto = item.css("title::text").get()
-            url = item.css("link::text").get()
-            fecha = item.css("pubDate::text").get()
-            id = item.css("guid::text").get()
+        secciones = ["I. Disposiciones generales", "III. Otras disposiciones"]
 
-            yield {
-            "id": id,
-            "fecha": fecha,
-            "url": url,
-            "texto_completo_boe": texto,
-            }
+        for h3 in response.css("h3"):
+            topic = h3.xpath("string()").get().strip()
+            if topic in secciones:
+                contenido = []
+                for sibling in h3.xpath("following-sibling::*"):
+                    if sibling.root.tag in ["h2", "h3"]:
+                        break
+                    contenido.append(sibling)
+
+                bloques = []
+                current_bloque ={}
+                for element in contenido:
+                    texto = element.xpath("string()").get().strip()
+
+                    if texto.isupper() and len(texto) < 100:
+                        if current_bloque:
+                            bloques.append(current_bloque)
+                        current_bloque = {
+                            "departamento": texto,
+                            "topic": None,
+                            "texto": ""
+                        }
+                    elif current_bloque and current_bloque["topic"] is None:
+                        current_bloque["topic"] = texto
+                    elif current_bloque:
+                        current_bloque["texto"] += texto + "\n"
+
+                if current_bloque:
+                    bloques.append(current_bloque)
+
+                for bloque in bloques:
+                    yield {
+                        "seccion": titulo,
+                        "departamento": bloque["departamento"],
+                        "topic": bloque["topic"],
+                        "texto": bloque["texto"].strip()
+                    }
