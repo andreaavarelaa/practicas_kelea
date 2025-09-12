f6a896f98b25975120aef0697f2c3fa94f16f64c
Andrea Varela Fernández <andrea.varela.fernandez@udc.es>
2025-09-12 10:06:45 +0200

    feat: traducción

--- boe/boe/spiders/boe_spider.py
+++ boe/boe/spiders/boe_spider.py
@@ -5,49 +5,49 @@ class BOESpider(scrapy.Spider):
     start_urls = ["https://boe.es/boe/dias/2025/09/10/"]
 
     def parse(self, response):
-        secciones = ["I. Disposiciones generales", "III. Otras disposiciones"]
+        sections = ["I. Disposiciones generales", "III. Otras disposiciones"]
         departments = ["MINISTERIO DE TRABAJO Y ECONOMÍA SOCIAL","MINISTERIO DE INCLUSIÓN, SEGURIDAD SOCIAL Y MIGRACIONES", "MINISTERIO DE HACIENDA", "MINISTERIO DE ECONOMÍA, COMERCIO Y EMPRESA",
                        "MINISTERIO DE INDUSTRIA Y TURISMO", "MINISTERIO DE DERECHOS SOCIALES, CONSUMO Y AGENDA 2030", "MINISTERIO PARA LA TRANSICIÓN ECOLÓGICA Y EL RETO DEMOGRÁFICO",
                        "MINISTERIO DE SANIDAD", "MINISTERIO DE TRANSPORTES Y MOVILIDAD SOSTENIBLE", "MINISTERIO PARA LA TRANSFORMACIÓN DIGITAL Y DE LA FUNCIÓN PÚBLICA", "BANCO DE ESPAÑA"]
 
         for h3 in response.css("h3"):
-            topic = h3.xpath("string()").get().strip()
-            if topic in secciones:
-                contenido = []
+            section = h3.xpath("string()").get().strip()
+            if section in sections:
+                content = []
                 for sibling in h3.xpath("following-sibling::*"):
                     if sibling.root.tag in ["h2", "h3"]:
                         break
-                    contenido.append(sibling)
+                    content.append(sibling)
 
-                bloques = []
-                current_bloque ={}
+                blocks = []
+                current_block ={}
 
-                for element in contenido:
-                    texto = element.xpath("string()").get().strip()
+                for element in content:
+                    text = element.xpath("string()").get().strip()
 
                     if element.root.tag == "h4":
-                        if current_bloque and current_bloque["departamento"] in departments:
-                            bloques.append(current_bloque)
+                        if current_block and current_block["department"] in departments:
+                            blocks.append(current_block)
 
-                        current_bloque = {
-                            "departamento": texto,
+                        current_block = {
+                            "department": text,
                             "topic": None,
-                            "texto": ""
+                            "text": ""
                         }
 
-                    elif current_bloque and current_bloque["topic"] is None:
-                        current_bloque["topic"] = texto
-                        
-                    elif current_bloque:
-                        current_bloque["texto"] += texto + "\n"
+                    elif current_block and current_block["topic"] is None:
+                        current_block["topic"] = text
 
-                if current_bloque and current_bloque["departamento"] in departments:
-                    bloques.append(current_bloque)
+                    elif current_block:
+                        current_block["text"] += text + "\n"
 
-                for bloque in bloques:
+                if current_block and current_block["department"] in departments:
+                    blocks.append(current_block)
+
+                for block in blocks:
                     yield {
-                        "seccion": topic,
-                        "departamento": bloque["departamento"],
-                        "topic": bloque["topic"],
-                        "texto": bloque["texto"].strip()
+                        "section": section,
+                        "department": block["department"],
+                        "topic": block["topic"],
+                        "text": block["text"].strip()
                     }
