9f168633e6d6f7adf6bd5f477c9edc776498a5a8
Andrea Varela Fernández <andrea.varela.fernandez@udc.es>
2025-09-15 11:53:29 +0200

    update: pipelines.py

--- boe/boe/pipelines.py
+++ boe/boe/pipelines.py
@@ -17,11 +17,11 @@ class BoePipeline:
 
     def open_spider(self, spider):
         self.connection = pymysql.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME,
+            host=os.getenv(DB_HOST),
+            port=int(os.getenv(DB_PORT)),
+            user=os.getenv(DB_USER),
+            password=os.getenv(DB_PASSWORD),
+            database=os.getenv(DB_NAME),
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
